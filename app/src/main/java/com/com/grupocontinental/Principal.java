5be10904de9a28446c3a846b562cc2778d5a8422
mihaw <mihaw-12@hotmail.com>
2023-11-20 19:47:53 -0600

    MAS INFORMACION 85%

--- app/src/main/java/com/com/grupocontinental/Principal.java
+++ app/src/main/java/com/com/grupocontinental/Principal.java
@@ -14,6 +14,8 @@ import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +27,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -38,7 +43,7 @@ import java.util.concurrent.Executors;
 
 public class Principal extends AppCompatActivity {
 
-    private LinearLayout div_recycler_autos,div_infromacion_auto;
+    private LinearLayout div_recycler_autos,ver_filtros,caja_filtros;
     private ExecutorService executorService;
     private JSONArray json_datos_autos;
     private Context context;
@@ -46,10 +51,14 @@ public class Principal extends AppCompatActivity {
     private RecyclerView recycler_autos;
     private AdadpterListaAutos adadpterListaAutos;
     private ArrayList<ListaAutos> listaAutosArrayList,listaAutosFiltrados;
-    private String vista_actual_str,palabra_buscada_str;
-    private ImageView buscar_palabra,borrar_palabra;
+    private String vista_actual_str,palabra_buscada_str,foto_1_str,foto_2_str,foto_3_str,numero_foto;
+    private ImageView buscar_palabra,borrar_palabra,cerrar_caja_filtros,imagen_auto,btn_imagen_2,btn_imagen_3;
     private EditText palabra_buscada;
     private SeekBar controlador_precio;
+    private RadioGroup grupo_filtros;
+    private TextView filtros_tv,marca_auto_tv,modelo_auto_tv,precio_auto_tv,comentarios_auto_tv;
+    private ScrollView div_infromacion_auto;
+    String url_server;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,7 +80,18 @@ public class Principal extends AppCompatActivity {
         borrar_palabra=findViewById(R.id.borrar_palabra);
         controlador_precio=findViewById(R.id.controlador_precio);
         SERVIDOR_CONTROLADOR = new Servidor().local;
-
+        ver_filtros=findViewById(R.id.ver_filtros);
+        caja_filtros=findViewById(R.id.caja_filtros);
+        grupo_filtros=findViewById(R.id.grupo_filtros);
+        filtros_tv=findViewById(R.id.filtros_tv);
+        cerrar_caja_filtros=findViewById(R.id.cerrar_caja_filtros);
+        imagen_auto=findViewById(R.id.imagen_auto);
+        marca_auto_tv=findViewById(R.id.marca_auto_tv);
+        modelo_auto_tv=findViewById(R.id.modelo_auto_tv);
+        precio_auto_tv=findViewById(R.id.precio_auto_tv);
+        comentarios_auto_tv=findViewById(R.id.comentarios_auto_tv);
+        btn_imagen_2=findViewById(R.id.btn_imagen_2);
+        btn_imagen_3=findViewById(R.id.btn_imagen_3);
         controlador_precio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -133,6 +153,209 @@ public class Principal extends AppCompatActivity {
                 palabra_buscada.setText("");
                 adadpterListaAutos = new AdadpterListaAutos(listaAutosArrayList);
                 recycler_autos.setAdapter(adadpterListaAutos);
+            }
+        });
+        ver_filtros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                caja_filtros.setVisibility(View.VISIBLE);
+            }
+        });
+        grupo_filtros.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.marca_menor:
+                        Collections.sort(listaAutosArrayList, new Comparator<ListaAutos>() {
+                            @Override
+                            public int compare(ListaAutos o1, ListaAutos o2) {
+                                // Parsea los valores de cobros a Double y compara
+                                String nombre1 = o1.getMarca();
+                                String nombre2 = o2.getMarca();
+                                Log.e("nombre1", nombre1);
+                                Log.e("nombre2", nombre2);
+                                return nombre1.compareTo(nombre2); // Ordena alfabéticamente de A a Z
+
+                            }
+                        });
+                        adadpterListaAutos = new AdadpterListaAutos(listaAutosArrayList);
+                        recycler_autos.setAdapter(adadpterListaAutos);
+                        // Notifica al adaptador para que se actualice con la nueva ordenación
+                        adadpterListaAutos.notifyDataSetChanged();
+                        caja_filtros.setVisibility(View.GONE);
+                        filtros_tv.setText("Marca A-Z");
+
+                        break;
+
+                    case R.id.marca_mayor:
+                        Collections.sort(listaAutosArrayList, new Comparator<ListaAutos>() {
+                            @Override
+                            public int compare(ListaAutos o1, ListaAutos o2) {
+                                String nombre1 = o1.getMarca();
+                                String nombre2 = o2.getMarca();
+                                Log.e("nombre1", nombre1);
+                                Log.e("nombre2", nombre2);
+                                return nombre2.compareTo(nombre1); // Ordena alfabéticamente de Z a A
+                            }
+                        });
+                        adadpterListaAutos = new AdadpterListaAutos(listaAutosArrayList);
+                        recycler_autos.setAdapter(adadpterListaAutos);
+                        // Notifica al adaptador para que se actualice con la nueva ordenación
+                        adadpterListaAutos.notifyDataSetChanged();
+                        caja_filtros.setVisibility(View.GONE);
+                        filtros_tv.setText("Marca Z-A");
+
+                        break;
+                    case R.id.kms_menor:
+                        Collections.sort(listaAutosArrayList, new Comparator<ListaAutos>() {
+                            @Override
+                            public int compare(ListaAutos o1, ListaAutos o2) {
+                                // Parsea los valores de cobros a Double y compara
+                                String fecha1 = o1.getKms();
+                                String fecha2 = o2.getKms();
+                                Log.e("fecha1", fecha1);
+                                Log.e("fecha1", fecha2);
+                                return fecha1.compareTo(fecha2); // Ordena alfabéticamente de A a Z
+
+                            }
+                        });
+                        adadpterListaAutos = new AdadpterListaAutos(listaAutosArrayList);
+                        recycler_autos.setAdapter(adadpterListaAutos);
+                        adadpterListaAutos.notifyDataSetChanged();
+                        caja_filtros.setVisibility(View.GONE);
+                        filtros_tv.setText("Kilometraje ascedente");
+
+                        break;
+
+                    case R.id.kms_mayor:
+                        Collections.sort(listaAutosArrayList, new Comparator<ListaAutos>() {
+                            @Override
+                            public int compare(ListaAutos o1, ListaAutos o2) {
+                                // Parsea los valores de cobros a Double y compara
+                                String fecha1 = o1.getKms();
+                                String fecha2 = o2.getKms();
+                                Log.e("fecha1", fecha1);
+                                Log.e("fecha2", fecha2);
+                                return fecha2.compareTo(fecha1);
+                            }
+                        });
+                        adadpterListaAutos = new AdadpterListaAutos(listaAutosArrayList);
+                        recycler_autos.setAdapter(adadpterListaAutos);
+                        // Notifica al adaptador para que se actualice con la nueva ordenación
+                        adadpterListaAutos.notifyDataSetChanged();
+                        caja_filtros.setVisibility(View.GONE);
+                        filtros_tv.setText("Kilometraje descendente");
+
+                        break;
+
+                    case R.id.precio_menor:
+                        Collections.sort(listaAutosArrayList, new Comparator<ListaAutos>() {
+                            @Override
+                            public int compare(ListaAutos o1, ListaAutos o2) {
+                                String deuda_actual1 = o1.getPrecio();
+                                String deuda_actual2 = o2.getPrecio();
+
+                                // Reemplazar valores vacíos con "0"
+                                if (deuda_actual1.isEmpty()) {
+                                    deuda_actual1 = "0";
+                                }
+                                if (deuda_actual2.isEmpty()) {
+                                    deuda_actual2 = "0";
+                                }
+
+                                double deuda1 = Double.parseDouble(deuda_actual1);
+                                double deuda2 = Double.parseDouble(deuda_actual2);
+                                Log.e("deuda1", String.valueOf(deuda1));
+                                Log.e("deuda2", String.valueOf(deuda2));
+                                return Double.compare(deuda1, deuda2);
+                            }
+                        });
+                        adadpterListaAutos = new AdadpterListaAutos(listaAutosArrayList);
+                        recycler_autos.setAdapter(adadpterListaAutos);
+                        adadpterListaAutos.notifyDataSetChanged();
+                        caja_filtros.setVisibility(View.GONE);
+                        filtros_tv.setText("Precio ascedente");
+
+                        break;
+
+                    case R.id.precio_mayor:
+                        Collections.sort(listaAutosArrayList, new Comparator<ListaAutos>() {
+                            @Override
+                            public int compare(ListaAutos o1, ListaAutos o2) {
+                                String deuda_actual1 = o1.getPrecio();
+                                String deuda_actual2 = o2.getPrecio();
+
+                                // Reemplazar valores vacíos con "0"
+                                if (deuda_actual1.isEmpty()) {
+                                    deuda_actual1 = "0";
+                                }
+                                if (deuda_actual2.isEmpty()) {
+                                    deuda_actual2 = "0";
+                                }
+
+                                double deuda1 = Double.parseDouble(deuda_actual1);
+                                double deuda2 = Double.parseDouble(deuda_actual2);
+                                Log.e("deuda1", String.valueOf(deuda1));
+                                Log.e("deuda2", String.valueOf(deuda2));
+                                return Double.compare(deuda2, deuda1);
+                            }
+                        });
+                        adadpterListaAutos = new AdadpterListaAutos(listaAutosArrayList);
+                        recycler_autos.setAdapter(adadpterListaAutos);
+                        adadpterListaAutos.notifyDataSetChanged();
+                        caja_filtros.setVisibility(View.GONE);
+                        filtros_tv.setText("Precio descendente");
+
+                        break;
+
+
+
+
+                    default:
+                        // No se ha seleccionado ningún RadioButton
+                        break;
+                }
+            }
+        });
+        cerrar_caja_filtros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                caja_filtros.setVisibility(View.GONE);
+
+            }
+        });
+        btn_imagen_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_imagen_2.setVisibility(View.VISIBLE);
+
+                if (numero_foto.equals("posicion_1")){
+                    Picasso.get().load(url_server+foto_2_str).into(imagen_auto);
+                    btn_imagen_2.setVisibility(View.GONE);
+                    numero_foto="posicion_2";
+                }
+                if (numero_foto.equals("posicion_3")){
+                    Picasso.get().load(url_server+foto_1_str).into(imagen_auto);
+                    btn_imagen_3.setVisibility(View.VISIBLE);
+
+                    numero_foto="posicion_1";
+                }
+            }
+        });
+        btn_imagen_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numero_foto.equals("posicion_1")){
+                    Picasso.get().load(url_server+foto_3_str).into(imagen_auto);
+                    numero_foto="posicion_3";
+                    btn_imagen_3.setVisibility(View.GONE);
+                }
+                if (numero_foto.equals("posicion_2")){
+                    Picasso.get().load(url_server+foto_1_str).into(imagen_auto);
+                    numero_foto="posicion_1";
+                    btn_imagen_2.setVisibility(View.VISIBLE);
+
+                }
             }
         });
     }
@@ -220,6 +443,16 @@ public class Principal extends AppCompatActivity {
         div_recycler_autos.setVisibility(View.GONE);
         div_infromacion_auto.setVisibility(View.VISIBLE);
         vista_actual_str="informacion_autos";
+        foto_1_str=foto_1;
+        foto_2_str=foto_2;
+        foto_3_str=foto_3;
+        numero_foto="posicion_1";
+        url_server="http://192.168.100.4/grupoContinental/vista/img/autos/";
+        Picasso.get().load(url_server+foto_1).into(imagen_auto);
+        marca_auto_tv.setText(marca);
+        modelo_auto_tv.setText(modelo);
+        precio_auto_tv.setText("Precio desde $"+precio);
+        comentarios_auto_tv.setText(comentarios);
         Log.e("marca",marca+modelo+kms);
     }
 
